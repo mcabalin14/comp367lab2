a79819ba1ca252667779d92218ac5032519bff39
Michael Angelo Cabalinan <mcabalin@my.centennialcollege.ca>
2025-02-24 16:43:17 -0500

    Initial Commit - Michael Lab 2 Web App

---
+++ michael_lab2/src/main/java/michael_lab2/Home.java
@@ -0,0 +1,15 @@
+package michael_lab2;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Home extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
